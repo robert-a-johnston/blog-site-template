9c82cd6f87b4b36cdd7c7393b154a9ab66da6dd0
ar[arnau321 <arnau123@att.net>
2022-01-03 09:52:57 -0500

    Move stuff to header/footer

--- footer.php
+++ footer.php
@@ -1,7 +1,13 @@
 <?php
-			wp_footer();
-		?>          
-	
+		wp_footer();
+?>          
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+  <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+  
+</footer>
+
+</div>
 	
 </body>
 </html> 
