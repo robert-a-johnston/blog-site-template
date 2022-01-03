51b7250de5b40a564ffc5f25a07ee7c11d38f759
ar[arnau321 <arnau123@att.net>
2022-01-03 10:05:45 -0500

    Make home page dynamic

--- front-page.php
+++ front-page.php
@@ -4,12 +4,23 @@
     
   
 	  
-		<article class="content px-3 py-5 p-md-5">
+	<article class="content px-3 py-5 p-md-5">
 	    
-	    </article>
+		<?php
+			if( have_posts() ) {
+
+				while( have_posts() ) {
+					the_post();
+					the_content();
+				}
+			}
+		?>
+
+
+	</article>
 	    
-  <?php
-		get_footer();
-	?>
+<?php
+	get_footer();
+?>
     
 
